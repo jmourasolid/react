790b5517ed1e4a3927ea58215738414045801a32
jmourasolid <139516502+jmourasolid@users.noreply.github.com>
2024-01-15 23:30:27 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Novo sistema Financeiro Embreve nossas mudancas</h1>
+      <h1>Vite + Novo sistema Nossa pagina WEB \o/</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
